2566532d7c23c42dbe7717b760016fae8fa83202
vpplag <74141971+vpplag@users.noreply.github.com>
2021-04-12 13:30:48 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    if (reverse == 1) {
+        reverse = 0
+    } else {
+        reverse = 1
+    }
+})
+let led2 = 0
+let reverse = 0
+Freenove.Initialization(DigitalPin.P0, DigitalPin.P1, DigitalPin.P2)
+reverse = 0
+basic.forever(function () {
+    for (let index = 0; index <= 7; index++) {
+        if (reverse == 0) {
+            led2 = index
+        } else {
+            led2 = 7 - index
+        }
+        Freenove.Write_Data(Freenove.Shift_Operators(1, shift_operator.left, led2), Order.MSBFIRST)
+        basic.pause(200)
+    }
+})
